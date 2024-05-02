24299d18e0518a58987bd4960bed31bd775e58d3
SabtainAliWazir <sabtainalicoder@gmail.com>
2024-05-02 15:13:31 +0500

    querry scope
    find title with local query scope

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Book::factory(33)->create()->each(function($book){
+            $num_reviews = random_int(5,30);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            Review::factory()->count($num_reviews)->good()->for($book)->create();
+        });
+
+        Book::factory(33)->create()->each(function($book){
+            $num_reviews = random_int(5,30);
+
+            Review::factory()->count($num_reviews)->average()->for($book)->create();
+        });
+
+        Book::factory(34)->create()->each(function($book){
+            $num_reviews = random_int(5,30);
+
+            Review::factory()->count($num_reviews)->bad()->for($book)->create();
+        });
     }
 }
